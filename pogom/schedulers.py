49d67998fe0748610ad0f1c6b888c40a3f16786f
marbink <mban.ipt@gmail.com>
2016-11-27 14:43:14 +0100

    Add --unv functionality

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -227,7 +227,10 @@ class HexSearchSpawnpoint(HexSearch):
     # Extend the generate_locations function to remove locations with no spawnpoints.
     def _generate_locations(self):
         n, e, s, w = hex_bounds(self.scan_location, self.step_limit)
-        spawnpoints = set((d['latitude'], d['longitude']) for d in Pokemon.get_spawnpoints(s, w, n, e))
+        if self.args.only_unvalid:
+            spawnpoints = set((d['latitude'], d['longitude']) for d in Pokemon.get_spawnpoints(s, w, n, e) if d['time_detail'] == -1)
+        else:
+            spawnpoints = set((d['latitude'], d['longitude']) for d in Pokemon.get_spawnpoints(s, w, n, e))
 
         if len(spawnpoints) == 0:
             log.warning('No spawnpoints found in the specified area!  (Did you forget to run a normal scan in this area first?)')
--- pogom/utils.py
+++ pogom/utils.py
@@ -140,6 +140,8 @@ def get_args():
                         required=True)
     parser.add_argument('--skip-empty', help='Enables skipping of empty cells  in normal scans - requires previously populated database (not to be used with -ss)',
                         action='store_true', default=False)
+    parser.add_argument('-unv', '--only-unvalid', help='Skips spawnpoints with already a valid time. Use this to focus on remaining spawnpoints with unvalid time. (use with --skip-empty)',
+                        action='store_true', default=False)
     parser.add_argument('-C', '--cors', help='Enable CORS on web server.',
                         action='store_true', default=False)
     parser.add_argument('-D', '--db', help='Database filename for SQLite.',
